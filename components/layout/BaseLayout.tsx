ab16bc185f70b3509a5c9105d812ecd72f5a8b0a
mashood shah <mediafire334@gmail.com>
2023-02-12 11:49:50 +0500

    initial commit

---
+++ components/layout/BaseLayout.tsx
@@ -0,0 +1,22 @@
+
+import { Navbar } from "components";
+import { FunctionComponent, ReactNode } from "react";
+
+interface BaseLayoutProps {
+    children?: ReactNode;
+  }
+  
+const BaseLayout: FunctionComponent<BaseLayoutProps> = ({children}) => {
+    return(
+        <>
+          <Navbar/>
+          <div className="py-16 bg-gray-50 overflow-hidden min-h-screen">
+              <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
+                  {children}
+              </div>
+          </div>
+        </>
+    )
+} 
+
+export default BaseLayout;
---
+++ components/nft/list/index.tsx
@@ -0,0 +1,24 @@
+import { NftMeta } from "@_types/nft"
+import { FunctionComponent } from "react"
+import NftItem from "../item"
+
+type NftListProps = {
+    nfts: NftMeta[]
+}
+
+const NftList: FunctionComponent<NftListProps> = ({nfts}) => {
+    return (
+        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+            {nfts.map(nft => 
+                <div key={nft.image} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                    <NftItem
+                        item={nft}
+                    />
+                </div>
+            )}
+
+      </div>
+    )
+}
+
+export default NftList
