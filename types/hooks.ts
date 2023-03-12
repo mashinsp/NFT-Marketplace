ba3251066c44343881e4ecdee52440647dca90ab
mashood shah <mediafire334@gmail.com>
2023-03-12 14:46:51 +0500

    HookTypes

--- types/hooks.ts
+++ types/hooks.ts
@@ -8,10 +8,17 @@ export type Web3Dependencies = {
     ethereum: MetaMaskInpageProvider;
 }
 
-export type CryptoSWRResponse = SWRResponse;
-
-export type CryptoHandlerHook = (params: any) => CryptoSWRResponse;
-
-export type CryptoHookFactory = {
-    (d: Partial<Web3Dependencies>): void
+export type CryptoHookFactory <D = any, P = any> = {
+    (d: Partial<Web3Dependencies>): CryptoHandlerHook<D,P>
 }
+
+export type CryptoHandlerHook<D=any, p=any> = (params: string) => CryptoSWRResponse<D>;
+
+export type CryptoSWRResponse<D = any> = SWRResponse<D>;
+
+// shorter way too manipulate
+// using other method just in case to call these in other files
+
+// export type CryptoHookFactory <D = any, P = any> = {
+//     (d: Partial<Web3Dependencies>): (params: P ) => SWRResponse<D>;
+// }
