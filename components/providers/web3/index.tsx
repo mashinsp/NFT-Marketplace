51c4e0eeedd86775192488c35240e94229deee87
mashood shah <mediafire334@gmail.com>
2023-03-15 23:08:59 +0500

    setup hooks

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -1,6 +1,7 @@
 import { createContext, FunctionComponent, useContext, useEffect, useState } from "react";
 import { createDefaultState, loadContract, Web3State } from "./utils";
 import {ethers}  from "ethers";
+import { setupHooks } from "@hooks/web3/setupHooks";
 
 interface Props {
     children: React.ReactNode;
@@ -23,7 +24,8 @@ const Web3Provider: FunctionComponent<Props>= ({children}) => {
                 ethereum: window.ethereum,
                 provider, 
                 contract,
-                isLoading: false
+                isLoading: false,
+                hooks: setupHooks({ethereum: window.ethereum, provider, contract})
             })
         }
 
