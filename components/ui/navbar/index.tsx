91cbabdd736aeec2d0ab22f142b7662907865993
mashood shah <mediafire334@gmail.com>
2023-03-25 10:59:14 +0500

    hooks

--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -86,7 +86,7 @@ export default function Example() {
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src="https://i.pinimg.com/474x/af/97/f2/af97f22a09afcfe6eb69d2751c5cf762.jpg"
                         alt=""
                       />
                     </Menu.Button>
